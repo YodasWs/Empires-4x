41e748988d02b638c40c2e50d93b3913809fd629
Sam Grundman <samgrundman27@gmail.com>
2025-09-14 22:00:28 -0400

    Add skeleton blog section
    for ongoing devlog posting, coming soon

---
+++ src/pages/blog/ctrl.js
@@ -0,0 +1,16 @@
+yodasws.page('pageBlog').setRoute({
+	title: 'Blog',
+	canonicalRoute: '/blog/',
+	template(match, ...p) {
+		const path = p.join('/').replace(/\/+/g, '/').replace(/^\/|\/$/g, '').split('/').filter(p => p != '');
+		if (path.length === 0) {
+			return 'pages/blog/index.html';
+		}
+		return {
+			canonicalRoute: '/blog/' + path.join('/') + '/',
+			template: 'pages/blog/' + path.join('.') + '.html',
+		};
+	},
+	route: '/blog(/.*)*',
+}).on('load', () => {
+});
